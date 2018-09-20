f68c80bba29c0c0de14e1000bbb0beb6ce59f62d
Dagasson <mushuk-artur@mail.ru>
2018-09-20 18:07:09 +0300

    Some files added

--- Course_work/Course_work/Classes/Connect_data.cs
+++ Course_work/Course_work/Classes/Connect_data.cs
@@ -9,9 +9,13 @@ namespace Course_work
 {
     static public class Connect_data//Подключение здорового человека.
     {
-        static public string adminConnect = "Data Source = ASHENVALE;Integrated Security = false; User ID=IS_admin; Password=IS_admin; ApplicationIntent=ReadWrite;";
-        static public string userConnect = "Data Source = ASHENVALE;Integrated Security = false; User ID=IS_user; Password=IS_user;ApplicationIntent=ReadWrite;";
+//        static public string adminConnect = "Data Source = ASHENVALE;Integrated Security = false; User ID=IS_admin; Password=IS_admin; ApplicationIntent=ReadWrite;";
+//        static public string userConnect = "Data Source = ASHENVALE;Integrated Security = false; User ID=IS_user; Password=IS_user;ApplicationIntent=ReadWrite;";
         static public string defaultConnect = "Data Source = ASHENVALE; Initial Catalog = Course_work; Integrated Security = true;";
         static public string currentConnect;
+
+        static public string adminConnect = "Data Source = ASHENVALE; Initial Catalog = Course_work; Integrated Security = true;";
+        static public string userConnect = "Data Source = ASHENVALE; Initial Catalog = Course_work; Integrated Security = true;";
+
     }
 }
